77057891c2f8e8649f0cfa8e16ad935ea4a6381d
Koichi Yoshizaki <kyoshizaki@gmail.com>
2020-06-09 19:50:39 +0900

    :wrench: Add default settings for production env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  # config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -80,4 +81,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Rotate log files by time
+  config.logger = Logger.new("log/production.log", 'weekly')
 end
