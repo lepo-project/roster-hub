4ee8f34f88a419a5a2eeda876df7d8c7dab62202
Koichi Yoshizaki <kyoshizaki@gmail.com>
2024-03-01 15:46:47 +0900

    :wrench: Adjust some config files to rails 7.1 default

--- Gemfile
+++ Gemfile
@@ -1,10 +1,5 @@
 source 'https://rubygems.org'
 
-git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?('/')
-  "https://github.com/#{repo_name}.git"
-end
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 7.1.3'
 # Use Puma as the app server
@@ -13,6 +8,8 @@ gem 'puma', '~> 6.0'
 gem 'rack-cors'
 
 gem 'activerecord-import'
+# Reduces boot times through caching; required in config/boot.rb
+gem "bootsnap", require: false
 gem 'doorkeeper', '~> 5.6'
 gem 'doorkeeper-i18n'
 gem 'rubyzip'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
@@ -42,9 +45,10 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  # config.logger = ActiveSupport::Logger.new(STDOUT)
+  #   .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+  #   .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
